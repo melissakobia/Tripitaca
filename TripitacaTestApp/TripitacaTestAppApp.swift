1797889d20b7ea8062601258d5d046b8499b528a
melissakobia <melissakobia@e-kraal.com>
2023-01-23 12:07:00 +0300

    Initial Commit

---
+++ TripitacaTestApp/TripitacaTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripitacaTestAppApp.swift
+//  TripitacaTestApp
+//
+//  Created by Muthoni on 23/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TripitacaTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
